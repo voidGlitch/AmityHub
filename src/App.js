dee7b0ae352eeea233d65c13529e9114c44e9d46
voidGlitch <official.shreshthav@gmail.com>
2024-02-02 15:56:43 +0530

    Started the project idea

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import "./sass/App.css";
-import "rsuite/dist/rsuite.min.css";
+import React from 'react';
+import 'rsuite/dist/rsuite.min.css';
 // import SideNavb from "./Components/SideNavb";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import Login from "./Components/Login";
-import SignUp from "./Components/SignUp";
-
-const styles = {
-  padding: 20,
-  textAlign: "center",
-};
-
+import './styles/main.scss';
 const App = () => (
   <div>
-    {/* <SideNavb className="navbars" /> */}
-    {/* <div style={styles}>Hello</div> */}
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <h1>hello</h1>
   </div>
 );
 
